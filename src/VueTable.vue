b8ff481f5ae13c10acb881cff751b0036242aaee
barbosa <contacto@omarbarbosa.com>
2021-02-15 12:45:00 -0500

    Restore If conditional

--- src/VueTable.vue
+++ src/VueTable.vue
@@ -38,7 +38,7 @@
                         </th>
                     </tr>
                 </thead>
-                <tbody v-if="records && records.length > 0">
+                <tbody v-if="records.length > 0">
                     <tr v-for="(record, index) in records" :key="index" :id="index">
                         <slot name="record" :record="record"></slot>
                     </tr>
